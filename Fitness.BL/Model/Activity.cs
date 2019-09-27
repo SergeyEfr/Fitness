97e07b4ab0cd8af841719ba265f50227e827c78a
Сергей <liamneeson7@mail.ru>
2019-09-27 23:29:36 +0300

    Добавлены Activity, Exercise и тест

---
+++ Fitness.BL/Model/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    [Serializable]
+    public class Activity
+    {
+        //Виды активности выступают в качестве справочника:
+        //1.Название вида активности
+        public string Name { get; }
+        //2.Количество сжигаемых калорий в единицу времени
+        public double CaloriesPerMinute { get; }
+
+        public Activity(string name, double caloriesPerMinute)
+        {
+            //Проверка
+             
+            Name = name;
+            CaloriesPerMinute = caloriesPerMinute;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.BL/Model/Exercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    [Serializable]
+    public class Exercise
+    {
+        //Время начала упражнения 
+        public DateTime Start { get; }
+        //Время конца упржанения
+        public DateTime Finish { get; }
+        //Вид упражнения
+        public Activity Activity { get; }
+        //Кто делал упражнение
+        public User User { get; }
+        public Exercise (DateTime start, DateTime finish, Activity activity, User user)
+        {
+            //Проверка
+
+            Start = start;
+            Finish = finish;
+            Activity = activity;
+            User = user;
+        }
+    }
+}
